f438a61c81cd3251a6b7865c5545b44e5cf165c3
shan-star <68067000+shan-star@users.noreply.github.com>
2022-07-19 08:38:29 +0800

    Update Home.vue

--- blog-app/src/Home.vue
+++ blog-app/src/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="home">
-    <Head-photo v-show="isShow && ($store.state.token == null) && $route.path=='/' " @mainPageClick="showMainPage"/>
+    <Head-photo v-show="isShow && ($store.state.token == null) && $route.path=='/' && focusMode"
+                @mainPageClick="showMainPage"/>
 
-    <el-container class="index-container">
+    <el-container :class="{'active':focusMode}">
       <base-header :activeIndex="activeIndex" @click="showMainPage"></base-header>
-        <router-view class="me-container"/>
-			<base-footer v-show="footerShow"></base-footer>
-		</el-container>
+      <router-view class="me-container"/>
+      <base-footer v-show="footerShow"></base-footer>
+    </el-container>
 
   </div>
 
@@ -19,51 +20,62 @@ import HeadPhoto from "./views/HeadPhoto";
 
 export default {
   name: 'Home',
-  data (){
-  	return {
-  			activeIndex: '/',
-  			footerShow:true,
-        isShow: true,
-  	}
-  },
-  components:{
-    HeadPhoto,
-  	'base-header':BaseHeader,
-  	'base-footer':BaseFooter
-  },
-  methods:{
-    showMainPage(){
-      this.isShow = false;
+  data() {
+    return {
+      activeIndex: '/',
+      footerShow: true,
+      isShow: true,
+      focusMode: true
     }
   },
-  beforeRouteEnter (to, from, next){
-  	 next(vm => {
-    	vm.activeIndex = to.path
-  	})
+  components: {
+    HeadPhoto,
+    'base-header': BaseHeader,
+    'base-footer': BaseFooter
   },
-  beforeRouteUpdate (to, from, next) {
-	  if(to.path == '/'){
-	  	this.footerShow = true
-	  }else{
-	  	this.footerShow = false
-	  }
-	  this.activeIndex = to.path
-	  next()
-	}
+  mounted() {
+    if (this.getIsPhone()) {
+      this.focusMode = false
+    }
+  },
+  methods: {
+    showMainPage() {
+      this.isShow = false;
+    },
+    //检查是否为手机端
+    getIsPhone() {
+      let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+      return flag;
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.activeIndex = to.path
+    })
+  },
+  beforeRouteUpdate(to, from, next) {
+    if (to.path == '/') {
+      this.footerShow = true
+    } else {
+      this.footerShow = false
+    }
+    this.activeIndex = to.path
+    next()
+  }
 }
 </script>
 
 <style>
 
-.index-container {
+.active {
   min-height: 100vh;
-  background-image: url("https://niu.yilele.site/siye.jpg")!important; ;
+  background-image: url("https://niu.yilele.site/siye.jpg") !important;;
   background-repeat: repeat-x;
-  background-attachment:fixed;
-  background-size:cover;
+  background-attachment: fixed;
+  background-size: cover;
 }
 
-.me-container{
+.me-container {
   margin: 100px auto 140px;
 }
 </style>
